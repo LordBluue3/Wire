854a7dbb95bd1ad978bc9e509cec7c6a78f7a982
LordBluue3 <58037508+LordBluue3@users.noreply.github.com>
2022-09-17 18:17:53 -0300

    Função de redirecionamento

---
+++ js/redirecionar.js
@@ -0,0 +1,9 @@
+function redirecionar_galeria(){
+    window.location.href="../telas/produtos.html"
+}
+function redirecionar_galeria_produtos(){
+    window.location.href="../telas/galeria_produtos.html"
+}
+function redirecionar_blog(){
+    window.location.href="../telas/blog2.html"
+}
