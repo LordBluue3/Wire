532f9eeb0427b9a498b648bdec069601b9cdc86c
LordBluue3 <58037508+LordBluue3@users.noreply.github.com>
2022-09-17 19:11:52 -0300

    Arrumando os links das imagens

--- js/redirecionar.js
+++ js/redirecionar.js
@@ -1,9 +1,9 @@
 function redirecionar_galeria(){
-    window.location.href="../telas/produtos.html"
+    window.location.href="telas/produtos.html"
 }
 function redirecionar_galeria_produtos(){
-    window.location.href="../telas/galeria_produtos.html"
+    window.location.href="telas/galeria_produtos.html"
 }
 function redirecionar_blog(){
-    window.location.href="../telas/blog2.html"
+    window.location.href="telas/blog2.html"
 }
